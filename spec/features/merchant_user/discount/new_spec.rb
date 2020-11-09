0806209f631ef4869d874739526a6079d3e962c1
Garrett Cottrell <garrett.cottrell2116@gmail.com>
2020-11-08 18:17:33 -0700

    almost done with all tests

--- spec/features/merchant_user/discount/new_spec.rb
+++ spec/features/merchant_user/discount/new_spec.rb
@@ -93,5 +93,37 @@ RSpec.describe 'merchants discount new page', type: :feature do
       expect(current_path).to eq('/merchant/discount/new')
       expect(page).to have_content("Percent discount can't be blank")
     end
+
+    it 'I can create multiple discounts for the same merchant and they will
+    all be stored in the database' do
+      print_shop = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      bike_shop = Merchant.create(name: "Meg's Bike Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      merchant_user = print_shop.users.create!(name: 'JakeBob',
+        address: '124 Main St',
+        city: 'Denver',
+        state: 'Colorado',
+        zip: '80202',
+        email: 'Bob1234@hotmail.com',
+        password: 'heftybags',
+        password_confirmation: 'heftybags',
+        role: 1
+      )
+
+      discount_1 = print_shop.discounts.create(quantity: 5, percent_discount: 10)
+      discount_2 = print_shop.discounts.create(quantity: 4, percent_discount: 9)
+      discount_3 = print_shop.discounts.create(quantity: 3, percent_discount: 8)
+      discount_4 = print_shop.discounts.create(quantity: 2, percent_discount: 7)
+      discount_5 = print_shop.discounts.create(quantity: 1, percent_discount: 2)
+      discount_6 = bike_shop.discounts.create(quantity: 4, percent_discount: 43)
+      discount_7 = bike_shop.discounts.create(quantity: 10, percent_discount: 5)
+
+      visit '/login'
+      fill_in :email, with: 'Bob1234@hotmail.com'
+      fill_in :password, with: 'heftybags'
+      click_button 'Login'
+
+      expect(print_shop.discounts.count).to eq(5)
+      expect(bike_shop.discounts.count).to eq(2)
+    end
   end
 end
